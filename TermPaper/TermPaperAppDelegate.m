3dfb95d921d1ea45bfb03a1e5d0102396f92afb5
daltman <daltman@Don-Altmans-MacBook-Pro-2.local>
2014-02-26 13:26:55 -0800

    Message root view controller to save active paper on suspend event. in order to save contents of text view to file.

--- TermPaper/TermPaperAppDelegate.m
+++ TermPaper/TermPaperAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "TermPaperAppDelegate.h"
 #import "TermPaperModel.h"
+#import "TermPaperTextViewController.h"
 #import <DropboxSDK/DropboxSDK.h>
 #import <Crashlytics/Crashlytics.h>
 
@@ -46,7 +47,7 @@
 {
 	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-	[[TermPaperModel activeTermPaper] save];
+	[(TermPaperTextViewController *)[[[UIApplication sharedApplication] keyWindow] rootViewController] saveActivePaper];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
